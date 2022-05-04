a5d0aba582757a1b089f47898107c4312c6fa791
P1zzAITU <Aidar_amankeldy2004@mail.ru>
2022-05-04 12:09:20 +0600

    HashTable and BST were initiated, BST has put method

---
+++ .idea/assignment3/MyBinarySearchTree.java
@@ -0,0 +1,55 @@
+package assignment3;
+// search
+// insert
+// delete
+// inorder traversal
+public class MyBinarySearchTree<K extends Comparable<K>, V> {
+    private Node root;
+    private class Node {
+        private K key;
+        private V value;
+        private Node left, right;
+        public  Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public Node(K key) {
+            this.key = key;
+        }
+    }
+
+    public void put(K key, V value) {
+        root = insert(key, value);
+    }
+
+    private Node insert(K key, V value) {
+        if (root == null) {
+            root = new Node(key);
+            return root;
+        }
+
+        if (key.compareTo(root.key) < 0) {
+            root.left = insert(key, (V) root.left);
+        }
+
+        else if (key.compareTo(root.key) > 0) {
+            root.right = insert(key, (V) root.right);
+        }
+
+        return root;
+    }
+
+
+    public V get(K key) {
+        return null;
+    }
+
+    public Node delete(K key) {
+        return null;
+    }
+
+    public Iterable<K> iterator() {
+        return null;
+    }
+}
---
+++ .idea/assignment3/MyHashTable.java
@@ -0,0 +1,47 @@
+package assignment3;
+
+public class MyHashTable<K, V> {
+    private class HashNode<K, V> {
+        private K key;
+        private V value;
+        private HashNode<K, V> next;
+
+        public HashNode(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public String toString() {
+            return "{" + key + " " + value + "}";
+        }
+    }
+    private HashNode<K, V>[] chainArray;
+    private int M = 11;
+    private int size;
+
+    public MyHashTable() {
+
+    }
+    public MyHashTable(int M) {
+
+    }
+    private int hash(K key) {
+        return 0;
+    }
+    private void put(K key, V value) {
+
+    }
+    public V get(K key) {
+        return null;
+    }
+    public V remove(K key) {
+        return null;
+    }
+    public boolean contains(V value) {
+        return true;
+    }
+    public K getKey(V value) {
+        return null;
+    }
+}
